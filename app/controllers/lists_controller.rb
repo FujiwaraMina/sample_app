3e92e9fd32050c5b2779aace9a0adfd1206051b6
EC2 Default User <ec2-user@ip-172-31-39-55.ap-northeast-1.compute.internal>
2022-09-02 04:11:26 +0000

    [Fnish]app_9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,11 +7,14 @@ class ListsController < ApplicationController
   #以下を追加
   def create
     #1.&2.データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     #3.データをデータベースに保存するためのsaveメソッド実行
-    list.save
+     if @list.save
     #詳細画面面へリダイレクト
-    redirect_to list_path(list.id)
+    redirect_to list_path(@list.id)
+     else
+    render :new
+     end
   end
 
   def index
@@ -22,9 +25,25 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/edit'
+
   get 'top' =>'homes#top'
-  post 'lists' => 'lists#create' #ここを追記します
-  get 'lists' => 'lists#index'
-  #.../lists/1や.../lists/3に該当する
-  get 'lists/:id' => 'lists#show',as:'list'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists
 end
